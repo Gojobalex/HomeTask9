041efe521675811512698dca9014a28acbdeca99
Alexsandr <alunev.work@gmail.com>
2021-08-05 13:57:18 +0300

    Added hometask project2

--- src/test/java/test/PracticeForm.java
+++ src/test/java/test/PracticeForm.java
@@ -1,47 +1,95 @@
 package test;
 
 import com.codeborne.selenide.Configuration;
+import com.github.javafaker.Faker;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byClassName;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
-public class PracticeForm {
-    @BeforeAll
+public class PracticeForm extends TestBase {
+    Faker faker = new Faker();
 
-    static void setup() {
-        Configuration.startMaximized = true;
-    }
-    @Test
-    void PractiveFormTest() {
-        open("https://demoqa.com/automation-practice-form");
-        $("[id=firstName]").setValue("Ivan");
-        $("[id=lastName]").setValue("Ivanov");
-        $("[id=userEmail]").setValue("test@test.ru");
-        $("[for='gender-radio-1']").click();
-        $("[id=userNumber]").setValue("8800535335");
-        $(".react-datepicker-wrapper").click();
-        $(".react-datepicker__year-select").click();
-        $(".react-datepicker__year-select").selectOption("2005");
-        $(".react-datepicker__month-select").click();
-        $(".react-datepicker__month-select").selectOption("May");
-        $("[aria-label='Choose Thursday, May 12th, 2005']").click();
+    String firstName = faker.name().firstName(),
+            lastName = faker.name().lastName(),
+            email = faker.internet().emailAddress(),
+            gender = "Other",
+            mobile = faker.number().digits(10),
+            dayOfBirth = "10",
+            monthOfBirth = "May",
+            yearOfBirth = "1988",
+            subject1 = "Chemistry",
+            subject2 = "Commerce",
+            hobby1 = "Sports",
+            hobby2 = "Reading",
+            hobby3 = "Music",
+            picture = "1.png",
+            currentAddress = faker.address().fullAddress(),
+            state = "Uttar Pradesh",
+            city = "Merrut";
 
-        $("[id=subjectsInput]").setValue("Text").pressEnter(); // Элемент найден правильно, текст не сохраняется в форме
-        $("[for='hobbies-checkbox-3']").click();
-        //      $("[id=uploadPicture]").uploadFile  //Загрузка файла
-        $("[id=currentAddress]").setValue("Moscow");
-        $("[id=react-select-3-input]").setValue("NCR").pressEnter();
-        $("[id=react-select-4-input]").setValue("Delhi").pressEnter();
-        $("[id=submit]").click();
 
-        $(".table-responsive").shouldHave(text("Ivan"),text("Ivanov"),text("test@test.ru"),
-                text("Male"),text("8800535335"),text("12 May,2005"),
-                text("Music"),text("Moscow"),text("NCR Delhi"));
+        @Test
+        void successfulFillFormTest() {
+            step("Open students registration form", () -> {
+                open("https://demoqa.com/automation-practice-form");
+                $(".practice-form-wrapper").shouldHave(text("Student Registration Form"));
+            });
 
-    }
+            step("Fill students registration form", () -> {
+                step("Fill common data", () -> {
+                    $("#firstName").val(firstName);
+                    $("#lastName").val(lastName);
+                    $("#userEmail").val(email);
+                    $("#genterWrapper").$(byText(gender)).click();
+                    $("#userNumber").val(mobile);
+                });
+                step("Set date", () -> {
+                    $("#dateOfBirthInput").clear();
+                    $(".react-datepicker__month-select").selectOption(monthOfBirth);
+                    $(".react-datepicker__year-select").selectOption(yearOfBirth);
+                    $(".react-datepicker__day--0" + dayOfBirth).click();
+                });
+                step("Set subjects", () -> {
+                    $("#subjectsInput").val(subject1);
+                    $(".subjects-auto-complete__menu-list").$(byText(subject1)).click();
+                    $("#subjectsInput").val(subject2);
+                    $(".subjects-auto-complete__menu-list").$(byText(subject2)).click();
+                });
+                step("Set hobbies", () -> {
+                    $("#hobbiesWrapper").$(byText(hobby1)).click();
+                    $("#hobbiesWrapper").$(byText(hobby2)).click();
+                    $("#hobbiesWrapper").$(byText(hobby3)).click();
+                });
+
+                step("Set address", () -> {
+                    $("#currentAddress").val(currentAddress);
+                    $("#state").scrollTo().click();
+                    $("#stateCity-wrapper").$(byText(state)).click();
+                    $("#city").click();
+                    $("#stateCity-wrapper").$(byText(city)).click();
+                });
+                step("Submit form", () ->
+                        $("#submit").click());
+            });
+
+            step("Verify successful form submit", () -> {
+                $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
+                $x("//td[text()='Student Name']").parent().shouldHave(text(firstName + " " + lastName));
+                $x("//td[text()='Student Email']").parent().shouldHave(text(email));
+                $x("//td[text()='Gender']").parent().shouldHave(text(gender));
+                $x("//td[text()='Mobile']").parent().shouldHave(text(mobile));
+                $x("//td[text()='Date of Birth']").parent().shouldHave(text(dayOfBirth + " " + monthOfBirth + "," + yearOfBirth));
+                $x("//td[text()='Subjects']").parent().shouldHave(text(subject1 + ", " + subject2));
+                $x("//td[text()='Hobbies']").parent().shouldHave(text(hobby1 + ", " + hobby2 + ", " + hobby3));
+                $x("//td[text()='Address']").parent().shouldHave(text(currentAddress));
+                $x("//td[text()='State and City']").parent().shouldHave(text(state + " " + city));
+            });
+
+        }
 
 }
